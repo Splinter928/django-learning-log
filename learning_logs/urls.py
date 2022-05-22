26c53110dee0668dbfc7c222147ae90c29f5323e
Pavel Ivanov <splinter928@gmail.com>
2022-05-22 18:04:48 +0300

    commit 22_05_2022_1

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('topics/<int:topic_id>/', views.topic, name='topic'),  # Page with detailed information for current topic
     path('new_topic/', views.new_topic, name='new_topic'),  # Page for adding new topic
     path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),  # Page for adding new entry
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),  # Page for entry editing
 ]
